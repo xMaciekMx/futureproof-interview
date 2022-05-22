27428961eebebe61ce4fb90368ad90e067643446
Maciej Mesjasz <maciekmesjasz59@gmail.com>
2022-05-22 21:19:54 +0200

    small code reformat

--- app/app.py
+++ app/app.py
@@ -9,15 +9,21 @@ from bs4 import BeautifulSoup
 def handler(event, context):
     print("[INFO] Starting")
     bucket_name = os.getenv("BUCKET_NAME")
-    # Scrapping web
+
+    # Scrapping website
+
     requested_website = requests.get("https://coinmarketcap.com/currencies/bitcoin/").text
     soup = BeautifulSoup(requested_website, 'html.parser')
     price_div = soup.find("div", class_="priceValue")
     print("[INFO] Website parsed, proceeding to data modification")
-    # # Data modification
+
+    # Data modification
+
     price = price_div.get_text().strip()[1:].replace(',', "")
     date = str(datetime.datetime.today())[:-7].split()
+
     # Editing html template
+
     html_file = open("indexTemplate.html", "r")
     lines = html_file.readlines()
     html_file.close()
@@ -27,12 +33,14 @@ def handler(event, context):
     """
 
     # Saving modified html file
+
     print("[INFO] Saving modified html template")
     with open("/tmp/modifiedHtml.html", "a+") as new_html_file:
         new_html_file.writelines(lines)
     new_html_file.close()
 
     # Uplading html file to s3
+
     print("[INFO] Uploading to s3")
     s3_client = boto3.client('s3')
     try:
